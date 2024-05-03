94a0d881f844cd2295d09b04d6e9dcc21b9a9223
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-03 22:25:58 +0800

    [context:RoutePath] Add context-based routing

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,3 +1,25 @@
-export default function Route({url_path, children}: {url_path: string, children: React.ReactNode}){
-    return window.location.pathname.match(url_path) ? children : null;
+import { createContext} from "react";
+
+// export default function Route({url_path, children}: {url_path: string, children: React.ReactNode}){
+//     return window.location.pathname.match(url_path) ? children : null;
+// }
+
+type RoutePathMap = {
+    'url': string | null,
+    'matcher': RegExpMatchArray | null
+}
+
+export const RoutePathContext = createContext<RoutePathMap>({
+    'url': null,
+    'matcher': null
+})
+
+export function RoutePathProvider({urlPattern, children}: {urlPattern: string, children: React.ReactNode}){
+    const urlMatcher = window.location.pathname.match(urlPattern)
+    return (
+        urlMatcher !== null
+        && <RoutePathContext.Provider value={{url: urlPattern, matcher: urlMatcher}}>
+            {children}
+        </RoutePathContext.Provider>
+    )
 }
