8cc801aa44736ffaf13ef99e63ae302bfadad0ba
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-03 22:25:11 +0800

    [page:Blog] Remove redundant `Layout`, use context for routing

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,31 @@
-import Layout from "../Layout";
+import { useContext } from "react";
+import { RoutePathContext } from "../../Route";
 
-export default function BlogPage({id}: {id: number}){
+// TODO: read data from json file
+const data = [{
+    'id': 1,
+    'title': 'Hello world',
+    'short_description': 'Hello',
+    'thumbnail_url': '#',
+  }, {
+    'id': 2,
+    'title': 'Hello world 2',
+    'short_description': 'Hello',
+    'thumbnail_url': '#',
+  }]
+
+export default function BlogPage(){
+    const routePathCOntext = useContext(RoutePathContext);
+    const {matcher} = routePathCOntext
+
+    const id = matcher?.groups?.blog_id;
+    const selected_data = data.filter((items) => items.id === Number(id || "-1"))
+    console.log(selected_data)
     return (
-        <Layout>
-            <p>{id}</p>
-        </Layout>
+        selected_data.map((item, index) => (
+            <div key={index}>
+                <p>{item.title}</p>
+            </div>
+        ))
     )
 }
