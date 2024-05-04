b7a4b95586f269cda40fadc9626b28d625ead48e
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-04 11:02:43 +0800

    [page:Blog] Change `data` value, add thumbnail

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,14 +4,14 @@ import { RoutePathContext } from "../../Route";
 // TODO: read data from json file
 const data = [{
     'id': 1,
-    'title': 'Hello world',
-    'description': 'Hello',
-    'thumbnail_url': '#',
+    'title': 'Snapdragon X Elite hadir pertengahan tahun 2024',
+    'description': `Snapdragon X Elite adalah salah satu lini terbaru Qualcomm. SoC ini digadang-gadang sebagai processor yang dapat menyaingi SoC Apple M3. Dengan adanya SoC ini, pilihan konsumen terhadap laptop berbasis ARM akan meningkat`,
+    'thumbnail_url': 'https://gadget.jagatreview.com/wp-content/uploads/2023/10/Snapdragon-X-Elite_Hero-Image.webp',
   }, {
     'id': 2,
-    'title': 'Hello world 2',
-    'description': 'Hello',
-    'thumbnail_url': '#',
+    'title': 'Rabbit R1 dapat dijalankan di Android',
+    'description': 'Rabbit R1 adalah sebuah perangkat yang digunakan untuk menjalankan tugas dasar dan memanfaatkan AI. Baru-baru ini, pihak Android Authority menemukan bahwa kode Rabbit R1 dapat dijalankan pada smartphone Android',
+    'thumbnail_url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPzGrAY86uH0vtaEScQluYxc9kjgk2jsr4wVPBdtlxpg&s',
   }]
 
 export default function BlogPage(){
@@ -24,15 +24,19 @@ export default function BlogPage(){
     
     // TODO: handle Invalid id
     return (
+        // TODO: maybe not using map?
+        // TODO: set selected blog UI in separate function
         isValidId && selected_data.map((item, index) => (
             <div key={index}>
                 <h3 className="text-center font-semibold text-xl">{item.title}</h3>
+                {/* Thumbnail */}
+                <div className="mx-5 my-2">
+                    <img src={item.thumbnail_url} alt={item.title} />
+                </div>
                 {/* Description */}
                 <div>
                     <p>{item.description}</p>
                 </div>
-
-                {/* Thumbnail */}
             </div>
             
         ))
