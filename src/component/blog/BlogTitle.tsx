695ace433e01a7cdc66e58c06d14dae877fc29f3
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-04 10:15:30 +0800

    [page:home] Center the preview title

--- src/component/blog/BlogTitle.tsx
+++ src/component/blog/BlogTitle.tsx
@@ -1,5 +1,5 @@
 export default function BlogTitle({title}: {title: string}){
     return (
-        <h3 className="font-semibold text-lg">{title}</h3>
+        <h3 className="font-semibold text-lg text-center">{title}</h3>
     )
 }
