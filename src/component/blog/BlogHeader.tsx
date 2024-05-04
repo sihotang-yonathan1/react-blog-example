9be0ae9166fa9426efda86de9fe97a9b804d9d59
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-04 10:43:15 +0800

    [component:BlogHeader] Add more margin in horizaontal axis (`mx-4`)

--- src/component/blog/BlogHeader.tsx
+++ src/component/blog/BlogHeader.tsx
@@ -1,6 +1,6 @@
 export default function BlogHeader({children}: {children: React.ReactNode}){
     return (
-        <div className="mb-2">
+        <div className="mb-2 mx-4">
             {children}
         </div>
     )
