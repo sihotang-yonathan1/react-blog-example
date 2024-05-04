b47f5ec1ad1867742cb5d0a5ed077db408e1f6db
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-04 10:42:40 +0800

    [component:BlogDescription] remove the max width and max height restriction

--- src/component/blog/BlogDescription.tsx
+++ src/component/blog/BlogDescription.tsx
@@ -1,6 +1,6 @@
 export default function BlogDescription({description}: {description: string}){
     return (
-        <div className="overflow-hidden text-ellipsis h-[100px] w-[100px]">
+        <div className="overflow-hidden text-ellipsis">
             <p className="text-ellipsis">{description}</p>
         </div>
     )
