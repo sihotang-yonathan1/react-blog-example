1049d8186ac149a287edd1306e24bfa0ac00e12d
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-05-04 11:03:57 +0800

    [component:BlogThumbnail] set thumbnail to center (vertically)

--- src/component/blog/BlogThumbnail.tsx
+++ src/component/blog/BlogThumbnail.tsx
@@ -1,6 +1,6 @@
 export default function BlogThumbnail({url, alt}: {url: string, alt: string}){
     return (
-        <div className="max-w-[200px]">
+        <div className="flex flex-col justify-center max-w-[200px]">
             <img src={url} alt={alt}/>
         </div>
     )
